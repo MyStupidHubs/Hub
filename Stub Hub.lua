e695a2b744f5d9f8c32bc6765056e48729bbe959
Stub Hub <110139840+StubHubScript@users.noreply.github.com>
2024-02-25 23:08:18 -0300

    Update Stub Hub.lua

--- Stub Hub.lua
+++ Stub Hub.lua
@@ -714,37 +714,23 @@ end)
 local Tab = Window:NewTab("U3BB")
 local Section = Tab:NewSection("Hitbox [Not Mine]")
 Section:NewSlider("Hitbox Expander", "It increases the NPCS hitbox", 1000, 0, function(s) -- 500 (MaxValue) | 0 (MinValue)
-local s = s
 local x = s
 local y = s
 local z = s
 
-local function AdjustCharacterPositionRelativeToHitbox(character, hitboxSize)
-    local rootPart = character:WaitForChild("HumanoidRootPart")
-    local currentPos = rootPart.Position
-    local halfSize = hitboxSize / 2
-
-    -- Determina a nova posição baseada na borda da hitbox
-    local newPos = Vector3.new(math.sign(currentPos.X) * halfSize.X, rootPart.Position.Y, math.sign(currentPos.Z) * halfSize.Z)
-    rootPart.Position = newPos
-end
-
 for i,v in pairs(workspace:GetDescendants()) do
-    if v.Name == "Humanoid" and v.Parent:FindFirstChild("HumanoidRootPart") ~= nil and v.Parent.Name ~= game.Players.LocalPlayer.Name then
-        local h = v.Parent.HumanoidRootPart
-        h.Size = Vector3.new(x, y, z)
-        h.Transparency = 0
-        v.Parent.HumanoidRootPart.CanCollide = false
-
-        -- Ajusta a posição do jogador para a borda da hitbox
-        AdjustCharacterPositionRelativeToHitbox(v.Parent, Vector3.new(x, y, z))
-    end
+  if v.Name == "Humanoid" and v.Parent:FindFirstChild("HumanoidRootPart") ~= nil and v.Parent.Name ~= game.Players.LocalPlayer.Name then
+    local h = v.Parent.HumanoidRootPart
+    h.Size = Vector3.new(x,y,z)
+    h.Transparency = 0
+    v.Parent.HumanoidRootPart.CanCollide = false
+  end
 end
 end)
 Section:NewButton("Back To Default", "NPCS hitbox returns to normal", function()
-local x = 3
-local y = 3
-local z = 3
+local x = s
+local y = s
+local z = s
 
 for i,v in pairs(workspace:GetDescendants()) do
    if v.Name == "Humanoid" and v.Parent:FindFirstChild("HumanoidRootPart") ~= nil and v.Parent.Name ~= game.Players.LocalPlayer.Name then
